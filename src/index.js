e2c99ffd0c346aad49aae6a9c921b47f8b3d7976
Fede Lacabaratz <lacarq@gmail.com>
2020-04-24 12:37:52 +0200

    add first commit with folder structure to begin with the project

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name={'OneMusic'}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
